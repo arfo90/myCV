7fec4675184a84c438644367249fa40296410b3f
Amireza Fatemi <arfo.amireza@gmail.com>
2016-07-10 21:33:50 +0200

    Make port and server host dynamic

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var http = require('http');
 var express = require("express");
 var app = express();
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
+var server_host = process.env.YOUR_HOST || '127.0.0.1';
+
 var port = process.env.PORT || 5000;
 
 // //Lets define a port we want to listen to
@@ -32,4 +35,9 @@ console.log('Booting..');
 console.log('Here we go..');
 console.log('Server is Started');
 
-app.listen(port, '127.0.0.1')
+
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+    console.log('Server host %d', server_host);
+});
+
