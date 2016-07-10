20234f08ddd995bae1bcf75dcfb7de8a7c405fc0
Amireza Fatemi <arfo.amireza@gmail.com>
2016-07-10 20:44:25 +0200

    Change server.js bootup method

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 var http = require('http');
-var express = require('express');
-var app = express.createServer();
 
-//Lets define a port we want to listen to
-const PORT=8080; 
+var express = require("express");
+var app = express();
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    request.render('about.html');
-    response.end('It Works!! Path Hit: ' + request.url);
-}
+// //Lets define a port we want to listen to
+// const PORT=8080; 
 
-//Create a server
-var server = http.createServer(handleRequest);
+// //We need a function which handles requests and send response
+// function handleRequest(request, response){
+//     request.render('about.html');
+//     response.end('It Works!! Path Hit: ' + request.url);
+// }
 
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+// //Create a server
+// var server = http.createServer(handleRequest);
+
+// //Lets start our server
+// server.listen(PORT, function(){
+//     //Callback triggered when server is successfully listening. Hurray!
+//     console.log("Server listening on: http://localhost:%s", PORT);
+// });
+
+   app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render(index);
 });
+console.log('Booting..');
+console.log('Here we go..');
+console.log('Server is Started');
+
+app.listen(8080, '127.0.0.1')
