4e15c547e7fafb6c401fa36850a7790a25363563
Amireza Fatemi <arfo.amireza@gmail.com>
2016-07-10 21:37:21 +0200

    quick port fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 var app = express();
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
-var server_host = process.env.YOUR_HOST || '127.0.0.1';
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 var port = process.env.PORT || 5000;
 
